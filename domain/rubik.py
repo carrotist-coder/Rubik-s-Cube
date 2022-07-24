f5f6bc95828f527feb8507dd0593b2d872cb37c9
Carrotist <romark99@gmail.com>
2022-07-24 12:16:06 +0300

    Deleted Const, added method right_letters(), define() finished

--- domain/rubik.py
+++ domain/rubik.py
@@ -3,28 +3,37 @@ class Cube:
         self.solved = False
         self.cube = []
         self.solution = []
+        self.colors = ['w', 'g', 'o', 'b', 'r', 'y']
 
     def define(self):
         msgs = ['White Side', 'Green Side', 'Orange Side', 'Blue Side', 'Red Side', 'Yellow Side']
         cube_scan = []
-        for i in range(0,5):
+        for i in range(0,6):
             ans = str()
             while len(ans)!=9:
                 write(msgs[i]+': ')
                 ans = input()
                 if len(ans)!=9:
                     write('Please, enter 9 cells of the '+msgs[i]+'.')
+                elif not(self.right_letters(ans)):
+                    write('Please, enter only '+str(self.colors)+' letters without spaces, other letters are not allowed.')
                 else:
                     break
             cube_scan.append(ans)
+
+    def right_letters(self, side):
+        for i in range(0,9):
+            if side[i] in self.colors:
+                continue
+            else:
+                return False
+        return True
 
 
     def check(self):
         pass
 
-class Const:
-    def __init__(self):
-        self.colors = ['w','g','o','b','r','y']
+
 
 def write(msg):
     print(msg)
