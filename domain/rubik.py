4ee13c25708de5a3a9f41f7192bd93fc838a03c1
Carrotist <romark99@gmail.com>
2022-07-24 12:49:49 +0300

    Added positions of centre, edges, corners

--- domain/rubik.py
+++ domain/rubik.py
@@ -5,8 +5,31 @@ class Cube:
         self.solution = []
         self.colors = ['w', 'g', 'o', 'b', 'r', 'y']
 
+        self.white = self.colors[0]
+        self.green = self.colors[1]
+        self.orange = self.colors[2]
+        self.blue = self.colors[3]
+        self.red = self.colors[4]
+        self.yellow = self.colors[5]
+
+        self.centre = 4
+        self.edge_u = 7
+        self.edge_l = 5
+        self.edge_r = 3
+        self.edge_d = 1
+        self.corner_ul = 8
+        self.corner_ur = 6
+        self.corner_dl = 2
+        self.corner_dr = 0
+        # positions of centre, edges, corners
+        # cube is being solved in accordance with yellow-up, green-front
+
+
+
     def define(self):
-        msgs = ['White Side', 'Green Side', 'Orange Side', 'Blue Side', 'Red Side', 'Yellow Side']
+        # data is defined in accordance with self.colors
+        write("Put the cube's white side up and green side at the front.")
+        msgs = ['White Side', 'Green Side', 'Orange Side', 'Blue Side', 'Red Side', 'Yellow Side (Green at the back)']
         cube_scan = []
         for i in range(0,6):
             while True:
@@ -31,8 +54,6 @@ class Cube:
 
     def check(self):
         pass
-
-
 
 def write(msg):
     print(msg)
